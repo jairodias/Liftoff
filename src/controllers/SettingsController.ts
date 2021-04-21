60ad30da63fbeaaf32d74e4f9a28c89e845ad961
jairodias <jairo.junior@credpago.com>
2021-04-21 14:58:17 -0300

    feat: created user validation

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -3,13 +3,19 @@ import { SettingsService } from "../services/SettingsService"
 
 class SettingsController {
   async create(request: Request, response: Response) {
-    const { chat, username } = request.body
+    try {
+      const { chat, username } = request.body
 
-    const settingsService = new SettingsService();
+      const settingsService = new SettingsService();
 
-    const settings = await settingsService.create({ chat, username })
+      const settings = await settingsService.create({ chat, username })
 
-    return response.json(settings)
+      return response.json(settings)
+    } catch (error) {
+      return response.status(400).json({
+        message: error.message
+      })
+    }
   }
 }
 
--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -10,6 +10,11 @@ class SettingsService {
   async create({ chat, username }: ISettingsCreate) {
     const settingsRepository = getCustomRepository(SettingsRepository)
 
+    const userAlreadyExists = await settingsRepository.findOne({ username })
+
+    if (userAlreadyExists)
+      throw new Error("User already exists!")
+
     const settings = settingsRepository.create({
       chat,
       username
